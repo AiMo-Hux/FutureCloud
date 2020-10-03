71dd76977f81affa98271c8c6524ed85937b45e1
AiMo-Hux <AiMo.Hux@aliyun.com>
2020-10-03 12:59:07 +0800

    x

---
+++ FC_Main.cxx
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main()
+{
+    //std::cout<<"测试"<<std::endl; :已测试GBK编码在本设备上的有效性。需要封装一个函数。
+
+}
---
+++ FC_main.hxx
@@ -0,0 +1,26 @@
+#ifndef FC_MAIN_HXX
+#define FC_MAIN_HXX
+#include <cstdlib>
+#include <iostream>
+#include <cstdio>
+
+namespace invalpha
+{
+namespace AiMo
+{
+class fcCore
+{
+public:
+    fcCore();
+private:
+
+protected:
+};
+fcCore::fcCore()
+{
+
+}
+}
+}
+
+#endif // FC_MAIN_HXX
